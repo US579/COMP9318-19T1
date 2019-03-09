f11cd5f1033166e11d77b016a6bf90f2a119a4ce
us579 <wzus579@gmail.com>
2019-03-09 16:00:13 +1100

    commit

---
+++ lab2/submission.py
@@ -0,0 +1,10 @@
+## import modules here 
+import pandas as pd
+import numpy as np
+
+
+
+################# Question 1 #################
+
+def v_opt_dp(x, num_bins):# do not change the heading of the function
+    pass # **replace** this line with your code
