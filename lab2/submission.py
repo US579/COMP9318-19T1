9900bc214868e5b75a4acb59f5f725fc5d370f45
us579 <wzus579@gmail.com>
2019-03-12 13:55:33 +1100

    commit

--- lab2/submission.py
+++ lab2/submission.py
@@ -29,85 +29,52 @@ def v_opt_dp(x, num_bins):# do not change the heading of the function
     path_matrix  = [[-1 for _ in range(len(x))] for _ in range(num_bins)]
 
     for i in range(1,num_bins+1):
-
         for j in range(len(x)):
-            set_path = []
             if num_bins - i <= j and len(x) - j >= i:
                 suffix = x[j:]
-                print('i**********',i)
-                print('suffix',suffix)
                 if i == 1:
                     cost = SSE(suffix)
                     cost_matrix[i-1][j] = cost
                     index_matrix[i-1][j] = suffix
                     path_matrix[i-1][j] = suffix
-
                 else:
                     set_path = []
                     if len(suffix) == i:
-                        lis_path = []
                         for ele in suffix:
-                            lis_path.append([ele])
-                        print('ele',lis_path)
-                        set_path.append(lis_path)
-                        print('&&&&&&&&&&',set_path)
+                            set_path.append([ele])
                         index_matrix[i-1][j] = suffix
                         path_matrix[i-1][j] = set_path
                         cost_matrix[i-1][j] = 0
-                        print('inde',index_matrix)
                     else:
                         lis_cost = []
-
                         for mid in range(1, len(suffix) -i + 2):
-
                             one_path = []
-                            print('mid: ',mid)
                             pre_suffix = suffix[:mid]
                             one_path.append(pre_suffix)
-                            print(pre_suffix)
                             suf_suffix = suffix[mid:]
-
                             pre_cost = SSE(pre_suffix)
                             num = index_matrix[i-2].index(suf_suffix)
-                            one_path.append(path_matrix[i-2][num])
-                            print('num',num)
-                            print(index_matrix[i-2].index(suf_suffix))
+                            print(path_matrix[i-2][num])
+                            if i > 2:
+                                one_path+=(path_matrix[i - 2][num])
+                            else:
+                                one_path.append(path_matrix[i-2][num])
                             cost = pre_cost + cost_matrix[i-2][num]
-                            print('cost',cost)
                             lis_cost.append(cost)
-                            print('lis_cost',lis_cost)
-
                             set_path.append(one_path)
-                            print('set_path',set_path)
-                            print('one_path', one_path)
                         cost_matrix[i-1][j] = min(lis_cost)
                         index_mincost_path = lis_cost.index(min(lis_cost))
                         path_matrix[i-1][j] = set_path[index_mincost_path]
-
                         index_matrix[i-1][j] = suffix
 
-                        #path_matrix[i-1][j] = set_path.index(index_mincost_path)
-                        print('index_matrix',index_matrix)
+    bins = path_matrix[num_bins-1][0]
+    return cost_matrix , bins
 
-                        print('path_matrix',path_matrix)
-
-                        print('lis_cost',cost_matrix)
-    print('cost_matirx', cost_matrix)
-
-
-
-
-
-
-
-
-
-
-
-#print(SSE(a))
 
 
 
     
 
-v_opt_dp(x,num_bins)
+a ,b =v_opt_dp(x,num_bins)
+print(a)
+print(b)
