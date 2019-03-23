ee5c570bc8d54f868e282c01bb384c9c59bd886d
us579 <wzus579@gmail.com>
2019-03-23 15:05:54 +1100

    commit

--- lab3/submission.py
+++ lab3/submission.py
@@ -3,12 +3,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plot
 
-def signoid(inX):
-    return 1.0 / (1 + np.exp(-inX))
 
-def logistic_regression(data, labels, weights, num_epochs, learning_rate): # do not change the heading of the function
-
-    print(data)
 
 data_file='./asset/a'
 
@@ -24,3 +19,23 @@ weights = np.zeros(3) # We compute the weight for the intercept as well...
 print(weights)
 num_epochs = 50000
 learning_rate = 50e-5
+print(labels)
+np.set_printoptions(threshold='nan')
+
+def signoid(inX):
+    return 1.0 / (1 + np.exp(-inX))
+
+def gradient_descent(data,labels):
+    w = np.ones((np.shape(data)[0],1))
+    for i in range(num_epochs):
+        w = w - learning_rate * data.transpose() * (signoid(data*w) -labels)
+    return w
+
+print(gradient_descent(data,labels))
+
+
+def logistic_regression(data, labels, weights, num_epochs, learning_rate): # do not change the heading of the function
+
+
+
+    print(data)
