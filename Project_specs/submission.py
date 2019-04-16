db5c47bedda1245c31d377c8f87895154c5e7dac
us579 <wzus579@gmail.com>
2019-04-16 14:30:05 +1000

    add_lab5

---
+++ Project_specs/submission.py
@@ -0,0 +1,15 @@
+# Import your files here...
+
+# Question 1
+def viterbi_algorithm(State_File, Symbol_File, Query_File): # do not change the heading of the function
+    pass # Replace this line with your implementation...
+
+
+# Question 2
+def top_k_viterbi(State_File, Symbol_File, Query_File, k): # do not change the heading of the function
+    pass # Replace this line with your implementation...
+
+
+# Question 3 + Bonus
+def advanced_decoding(State_File, Symbol_File, Query_File): # do not change the heading of the function
+    pass # Replace this line with your implementation...
---
+++ lab5/submission.py
@@ -0,0 +1,6 @@
+## import modules here
+import numpy as np
+################# Question 1 #################
+
+def hc(data, k):# do not change the heading of the function
+    pass # Replace this line with your implementation...
